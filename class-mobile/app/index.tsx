0094d6560621b4b5837a2d8630f0df7a13207e27
yheseul <kangyheseul@naver.com>
2024-11-26 15:34:35 +0900

    01-03-webview

--- class-mobile/app/index.tsx
+++ class-mobile/app/index.tsx
@@ -1,12 +1,8 @@
 import HelloWord from "@/components/section01/01-01-hello-world";
 import ReactNativeTags from "@/components/section01/01-02-react-native-tags";
+import WebViewPage from "@/components/section01/01-03-webview";
 import { View } from "react-native";
 
 export default function Home() {
-  return (
-    <View>
-      {/* <HelloWord /> */}
-      <ReactNativeTags />
-    </View>
-  );
+  return <WebViewPage />;
 }
---
+++ class-mobile/components/section01/01-03-webview/index.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, StatusBar } from "react-native";
+import { WebView } from "react-native-webview";
+
+export default function WebViewPage() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar translucent={false} />
+      <WebView source={{ uri: `https://www.naver.com/` }} />
+    </SafeAreaView>
+  );
+}
